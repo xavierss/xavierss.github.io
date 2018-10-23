61cebace32479dd81d1df0785c5e8da1fc85dbd5
xavierss <xavierss02@gmail.com>
2018-10-23 12:01:48 +0900

    openlayers map 추가

---
+++ src/components/Map.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import Map from 'ol/Map.js'
+import View from 'ol/View.js'
+import TileLayer from 'ol/layer/Tile.js'
+import OSM from 'ol/source/OSM.js'
+
+export default {
+  name: 'Map',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      map: null
+    }
+  },
+  mounted () {
+    const map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      target: 'map',
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    })
+    this.map = map
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#map {
+  width: 100%;
+  height: 100%;
+}
+</style>
